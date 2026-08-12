e2ce5221ae9d6e58a80fd4bf6b460e1ed0ff2144
Mariana Nunes dos Santos <mariana-nunes6@hotmail.com>
2026-08-12 18:57:13 +0000

    Terminada a atividade

--- Codificador/App.java
+++ Codificador/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Codificador cod = new CodificadorSimples();
+        Codificador cod = new CodificadorM();
 
         System.out.println("Codificador: "+cod.getNome());
         System.out.println("Versao: "+cod.getDataCriacao());
         System.out.println("Nivel de segurança: "+cod.getNivelSeguranca());
-        
+
         String texto = "Este e o string a ser codificado";
         String codificado = cod.codifica(texto);
         String decodificado = cod.decodifica(codificado);
--- Codificador/CodificadorM.java
+++ Codificador/CodificadorM.java
@@ -1,14 +1,16 @@
+import java.math.BigInteger;
 import java.time.LocalDate;
 
-public interface CodificadorM {
+public class CodificadorM implements Codificador{
     // Retorna o nome do codificador
+
     public String getNome(){
         return "Codificador da Mari";
     }
 
     // Retorna a data de imlementação do codificador (pode ser usado como versao)
     public LocalDate getDataCriacao(){
-        return LocalDate.of(2026, 08, 10);
+        return LocalDate.of(2026, 8, 10);
     }
 
     // Retorna o nivel de segurança do codificador (1 = muito baixo, 100 = muito alto)
@@ -18,9 +20,13 @@ public interface CodificadorM {
 
     // Recebe um string e retorna o correspondente codificado
     public String codifica(String str){
-        return 
+        String codificada = (new BigInteger(str.toString())).toString(36);
+        return codificada;
     }
 
     // Recebe um string codificado e retorna o correspondente decodificado
-    public String decodifica(String str);
+    public String decodifica(String str){
+        String decodificada = (new BigInteger(str,36)).toString();
+        return decodificada;
+    }
 }
